1a247b9c6f77527798d1e2d3e4cbb638084cdff6
Espadaclar <eneroborde8002@gmail.com>
2017-02-25 18:49:54 +0100

    0655 añadido mt obtenerHoraMasAccesos(), muestar por pantalla la hora con mayor nº de accesos en el servidor.

--- AnalizadorAccesosAServidor.java
+++ AnalizadorAccesosAServidor.java
@@ -68,7 +68,44 @@ public class AnalizadorAccesosAServidor
         }
     }
 
-    
+    /**
+     * Tener un método llamado òbtenerHoraMasAccesos que, a partir de los procesos hechos por el método anterior encuentra la 
+     *hora (solo la hora, sin tener en cuenta los minutos) a la que se producen más accesos al servidor. El método muestra por 
+     *pantalla dicha hora y la devuelve. En caso de que se invoque este método sin haberse invocado el anterior el método informa
+     *por pantalla de que no tiene datos y no hace nada más.            arrayDeHoras.length
+     */
+    public int obtenerHoraMasAccesos(){     
+        int solucion = -1; // ------------- devuele la hora que más entradas ha tenido el servidor.
+        if( !entradasServidor.isEmpty()){            
+            for(int i = 0; i < 24; i ++){ 
+                int valorI = i;//------------ representa cada una de las horas del día.
+                int contEntradas = 0; // ---- cuenta las veces que una hora esta repetida, que son las entradas realizadas en esa hora.
+                for(int pi = 0; pi < entradasServidor.size(); pi ++){// ------ pi, además, representa cada una de las entradas al servidor.
+                    int valorPi = entradasServidor.get(pi).getHora();// ------ almacena la hora en que se produce cada una de las entradas.
+                    if(entradasServidor.get(valorI).getHora() == valorPi){//--- compara la hora tomada en el 1º for, con las horas marcadas
+                        contEntradas ++;                                  //-- en cada una de las entradas, Si hay horas repetidas, las suma.
+                    }
+                }
+                // cada posición del ArrayList 'entradasEnHora' representa cada una de las horas, donde se almacena el nº veces que se repiten.
+                entradasEnHora.add(contEntradas);
+            }
+            int entradas = 0;// --------- almacena el mayor nº de entradas producido en una de las horas,
+            for(int ci = 0; ci < entradasEnHora.size() ; ci ++){// --- recorre la colección del nº de entradas por cada una de las horas.
+                if(entradasEnHora.get(ci) > entradas){
+                    entradas = entradasEnHora.get(ci);
+                    solucion = ci;
+                }
+                System.out.println(ci+ ".- " +entradasEnHora.get(ci));  
+            }
+            System.out.println(""); 
+            System.out.println("hora con más entradas.- " +solucion); 
+            System.out.println("=========================");     
+        }
+        else{
+            System.out.println("  _____ Sin datos ¡¡¡¡¡.-");
+        }
+        return solucion;
+    }
 
 }
 
